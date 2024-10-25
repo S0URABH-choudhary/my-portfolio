05d5f92d8f86019b56042bd892cf1f1055a82187
S0URABH_choudhary <subh7898765@gmail.com>
2024-10-25 12:50:53 +0530

    added avtar and some animations and light and dark mode functions

--- script.js
+++ script.js
@@ -1,8 +1,111 @@
 var main = document.querySelector(".main");
 var cursor = document.querySelector(".circle");
-var contact_button = document.querySelector(".navbar-right")
-var area = document.querySelector(".social-media" )
+var contact_button = document.querySelector(".contact-wrap")
+var area = document.querySelectorAll(".links" )
 var contactarea = document.querySelector(".contact-form")
+var toggle = document.querySelector("#checkbox")
+var body = document.querySelector("body")
+var text  = document.querySelector(".text")
+var skill = document.querySelectorAll(".skill-card")
+var avtarbackground = document.querySelector(".window")
+var sun = document.getElementById("sun")
+var moon = document.getElementById("moon")
+var cloud1 = document.getElementById("cloud-2")
+var cloud2 = document.getElementById("cloud-1")
+var stars = document.getElementById("stars")
+var bluprints = document.querySelector(".blueprint")
+
+gsap.to(moon,{
+    top:"20%",
+    opacity:1,
+    duration:1.5
+})
+gsap.to(stars,{
+    opacity:1,
+    duration:2,
+    delay:.25
+})
+
+
+
+toggle.addEventListener("click",() => {
+    if (!toggle.checked){
+        body.classList.add("light-theme")
+        gsap.to(avtarbackground,{
+            backgroundColor:"skyblue"
+        })
+        gsap.to(sun,{
+            top:"20%",
+            opacity:1,
+            duration:1.5
+        })
+        gsap.to(moon,{
+            top:"-20%",
+            opacity:0
+        })
+        gsap.to(stars,{
+            opacity:0
+        })
+        gsap.to(cloud1,{
+            left:"4%"
+        })
+        gsap.to(cloud2,{
+            left:"35%"
+        })
+    }else{
+        body.classList.remove("light-theme") 
+        gsap.to(avtarbackground,{
+            backgroundColor:"#2e4482d0"
+        })  
+        gsap.to(moon,{
+            top:"20%",
+            opacity:1,
+            duration:1.5
+        })
+        gsap.to(stars,{
+            opacity:1,
+            duration:2,
+            delay:.25
+        })
+        gsap.to(sun,{
+            top:"-20%",
+            opacity:0
+        })
+        gsap.to(cloud1,{
+            left:"-40%"
+        })
+        gsap.to(cloud2,{
+            left:"90%"
+        })
+    }
+})
+
+skill.forEach(skillelement => {
+   skillelement.addEventListener("mouseenter",function(){
+    gsap.to(cursor,{
+        scale:0,
+    })
+   }) 
+   skillelement.addEventListener("mouseleave",function(){
+    gsap.to(cursor,{
+        scale:1,
+    })
+   }) 
+});
+
+area.forEach(element => {
+   element.addEventListener("mouseenter",function(){
+    gsap.to(cursor,{
+        scale:0,
+    })
+   }) 
+   element.addEventListener("mouseleave",function(){
+    gsap.to(cursor,{
+        scale:1,
+    })
+   }) 
+});
+
 
 const movingball = () =>{
     main.addEventListener("mousemove", function(dets) {
@@ -17,12 +120,12 @@ const movingball = () =>{
 movingball()
 
 const shrink = () =>{
-   area.addEventListener("mouseenter",function(){
+   bluprints.addEventListener("mouseenter",function(){
         gsap.to(cursor,{
             scale:0,
         })
     })
-    area.addEventListener("mouseleave",function(){
+    bluprints.addEventListener("mouseleave",function(){
         gsap.to(cursor,{
             scale:1
         })
@@ -38,21 +141,40 @@ const shrink = () =>{
         })
     })
     contact_button.addEventListener("mouseenter",function(){
+        gsap.to(text,{
+            color:"var(--secondary-color)"
+        })
         gsap.to(cursor,{
             scale:0
         })
         gsap.to(contact_button,{
-            backgroundColor:"#209CEE"
+            backgroundColor:"var(--tertiary-color)",
         })
     })
     contact_button.addEventListener("mouseleave",function(){
+        gsap.to(text,{
+            color:"var(--primary-color)"
+        })
         gsap.to(cursor,{
             scale:1
         })
         gsap.to(contact_button,{
-            backgroundColor:"white"
+            backgroundColor:"var(--secondary-color)"
         })
     })
 }
 shrink()
 
+const  sendmail = () =>{
+    var params = {
+        name:document.getElementById("your-name").value,
+        email:document.getElementById("email-id").value,
+        message:document.getElementById("subject").value,
+    };
+    const serviceid = "service_a9ivaew";
+    const templateid = "template_gmluw2n";
+
+    emailjs.sent(serviceid,templateid,params)
+    
+    .catch(err=>console.log(err));
+}
