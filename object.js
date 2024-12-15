5272b96ec7c4cb54521dbc8892e4e63aa6e6f800
S0URABH_choudhary <subh7898765@gmail.com>
2024-12-15 13:31:40 +0530

    added new project section removed previous one

---
+++ object.js
@@ -0,0 +1,8 @@
+const data = {
+    items: [
+        { name: "Herbal Wonders", imageUrl: "./assets/herbal-wonders.webp", discription: "<H3>Herbal wonters</h3>- A modern web design project . It showcases a seamless blend of animations and user-friendly navigation </br><h4>Features</h4>-Dynamic Animations </br>-Clean Design </br> -Interactive Elements </br>-Modern Aesthetic ", livelink: "https://animatedwebsite-mocha.vercel.app/", codelink:"https://github.com/S0URABH-choudhary/animatedwebsite", stack:[ { class: "fa-brands fa-html5" },
+            { class: "fa-brands fa-css3-alt" },
+            { class: "fa-brands fa-js" }] },
+        { name: "emplooye management systen", imageUrl: "./asscets/pexels-pixabay-159110.jpg", discription: "this is green tea" }
+    ],
+};
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var main = document.querySelector(".main");
 var cursor = document.querySelector(".circle");
 var contact_button = document.querySelector(".contact-wrap")
-var area = document.querySelectorAll(".links" )
+var area = document.querySelectorAll(".links")
 var contactarea = document.querySelector(".contact-form")
 var toggle = document.querySelector("#checkbox")
 var body = document.querySelector("body")
-var text  = document.querySelector(".text")
+var text = document.querySelector(".text")
 var skill = document.querySelectorAll(".skill-card")
 var avtarbackground = document.querySelector(".window")
 var sun = document.getElementById("sun")
@@ -20,193 +20,193 @@ var contactform = document.getElementById("contact-form")
 
 
 
-gsap.to(moon,{
-    top:"20%",
-    opacity:1,
-    duration:1.5
+gsap.to(moon, {
+    top: "20%",
+    opacity: 1,
+    duration: 1.5
 })
-gsap.to(stars,{
-    opacity:1
+gsap.to(stars, {
+    opacity: 1
 })
 
 
 
-toggle.addEventListener("click",() => {
-    if (!toggle.checked){
+toggle.addEventListener("click", () => {
+    if (!toggle.checked) {
         body.classList.add("light-theme")
-        document.getElementById("snor").style.display="block"
-        gsap.to(avtarbackground,{
-            backgroundColor:"skyblue"
+        document.getElementById("snor").style.display = "block"
+        gsap.to(avtarbackground, {
+            backgroundColor: "skyblue"
         })
-        gsap.to(sun,{
-            top:"20%",
-            opacity:1,
-            duration:1.5
+        gsap.to(sun, {
+            top: "20%",
+            opacity: 1,
+            duration: 1.5
         })
-        gsap.to(moon,{
-            top:"-20%",
-            opacity:0
+        gsap.to(moon, {
+            top: "-20%",
+            opacity: 0
         })
-        gsap.to(stars,{
-            opacity:0
+        gsap.to(stars, {
+            opacity: 0
         })
-        gsap.to(cloud1,{
-            left:"4%"
+        gsap.to(cloud1, {
+            left: "4%"
         })
-        gsap.to(cloud2,{
-            left:"35%"
+        gsap.to(cloud2, {
+            left: "35%"
         })
-    }else{
-        body.classList.remove("light-theme") 
-        document.getElementById("snor").style.display="none"
-        gsap.to(avtarbackground,{
-            backgroundColor:"#2e4482d0"
-        })  
-        gsap.to(moon,{
-            top:"20%",
-            opacity:1,
-            duration:1.5
+    } else {
+        body.classList.remove("light-theme")
+        document.getElementById("snor").style.display = "none"
+        gsap.to(avtarbackground, {
+            backgroundColor: "#2e4482d0"
         })
-        gsap.to(stars,{
-            opacity:1,
-            duration:2,
-            delay:.25
+        gsap.to(moon, {
+            top: "20%",
+            opacity: 1,
+            duration: 1.5
         })
-        gsap.to(sun,{
-            top:"-20%",
-            opacity:0
+        gsap.to(stars, {
+            opacity: 1,
+            duration: 2,
+            delay: .25
         })
-        gsap.to(cloud1,{
-            left:"-40%"
+        gsap.to(sun, {
+            top: "-20%",
+            opacity: 0
         })
-        gsap.to(cloud2,{
-            left:"90%"
+        gsap.to(cloud1, {
+            left: "-40%"
+        })
+        gsap.to(cloud2, {
+            left: "90%"
         })
     }
 })
 
 skill.forEach(skillelement => {
-   skillelement.addEventListener("mouseenter",function(){
-    gsap.to(cursor,{
-        scale:0,
+    skillelement.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 0,
+        })
     })
-   }) 
-   skillelement.addEventListener("mouseleave",function(){
-    gsap.to(cursor,{
-        scale:1,
+    skillelement.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 1,
+        })
     })
-   }) 
 });
 
 area.forEach(element => {
-   element.addEventListener("mouseenter",function(){
-    gsap.to(cursor,{
-        scale:0,
+    element.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 0,
+        })
     })
-   }) 
-   element.addEventListener("mouseleave",function(){
-    gsap.to(cursor,{
-        scale:1,
+    element.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 1,
+        })
     })
-   }) 
 });
 
 
-const movingball = () =>{
-    main.addEventListener("mousemove", function(dets) {
-        gsap.to(cursor,{
-            x:dets.x,
-            y:dets.y,
-            ease:"back.out",
-            duration:1
+const movingball = () => {
+    main.addEventListener("mousemove", function (dets) {
+        gsap.to(cursor, {
+            x: dets.x,
+            y: dets.y,
+            ease: "back.out",
+            duration: 1
         })
     });
 }
 movingball()
 
-const shrink = () =>{
-   bluprints.addEventListener("mouseenter",function(){
-        gsap.to(cursor,{
-            scale:0,
+const shrink = () => {
+    bluprints.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 0,
         })
     })
-    bluprints.addEventListener("mouseleave",function(){
-        gsap.to(cursor,{
-            scale:1
+    bluprints.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 1
         })
     })
-   contactarea.addEventListener("mouseenter",function(){
-        gsap.to(cursor,{
-            scale:0,
+    contactarea.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 0,
         })
     })
-    contactarea.addEventListener("mouseleave",function(){
-        gsap.to(cursor,{
-            scale:1
+    contactarea.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 1
         })
     })
-    contact_button.addEventListener("mouseenter",function(){
-        gsap.to(text,{
-            color:"var(--secondary-color)"
+    contact_button.addEventListener("mouseenter", function () {
+        gsap.to(text, {
+            color: "var(--secondary-color)"
         })
-        gsap.to(cursor,{
-            scale:0
+        gsap.to(cursor, {
+            scale: 0
         })
-        gsap.to(contact_button,{
-            backgroundColor:"var(--tertiary-color)",
+        gsap.to(contact_button, {
+            backgroundColor: "var(--tertiary-color)",
         })
     })
-    contact_button.addEventListener("mouseleave",function(){
-        gsap.to(text,{
-            color:"var(--primary-color)"
+    contact_button.addEventListener("mouseleave", function () {
+        gsap.to(text, {
+            color: "var(--primary-color)"
         })
-        gsap.to(cursor,{
-            scale:1
+        gsap.to(cursor, {
+            scale: 1
         })
-        gsap.to(contact_button,{
-            backgroundColor:"var(--secondary-color)"
+        gsap.to(contact_button, {
+            backgroundColor: "var(--secondary-color)"
         })
     })
 }
 shrink()
 
 
-contactform.addEventListener('submit', async function(event) {
+contactform.addEventListener('submit', async function (event) {
     event.preventDefault(); // Prevent default form submission
-    
+
     const form = event.target;
     const formData = new FormData(form);
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    
-    if(document.getElementById("email-id").value !== "" && !emailPattern.test(document.getElementById("email-id").value)){
+
+    if (document.getElementById("email-id").value !== "" && !emailPattern.test(document.getElementById("email-id").value)) {
         shownotification(warningmsgemail)
-    }else if(document.getElementById("your-name").value === ""|| document.getElementById("email-id").value === ""|| document.getElementById("message").value === ""){
+    } else if (document.getElementById("your-name").value === "" || document.getElementById("email-id").value === "" || document.getElementById("message").value === "") {
         shownotification(warningmsg);
         return false;
-    }else{
+    } else {
         try {
-          const response = await fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            body: formData
-          });
-      
-          if (response.ok) {
-            // Show a success message or update the UI
-            shownotification(successmsg)
-            form.reset(); // Optional: Clear the form fields after submission
-          } else {
-            // Handle non-200 responses
-            Status.innerText = "Failed to send email. Please try again.";
-          }
-        }catch (error) {
+            const response = await fetch('https://api.web3forms.com/submit', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (response.ok) {
+                // Show a success message or update the UI
+                shownotification(successmsg)
+                form.reset(); // Optional: Clear the form fields after submission
+            } else {
+                // Handle non-200 responses
+                Status.innerText = "Failed to send email. Please try again.";
+            }
+        } catch (error) {
             console.error('Error:', error);
             Status.innerText = "An error occurred. Please try again.";
-          } 
+        }
     }
-  
-   
-  });
+
+
+});
 
 
 // =============================================custom notifition message======================================================
@@ -218,47 +218,96 @@ let errormsg = '<i class="fa-solid fa-circle-xmark"></i> invalid input, please c
 
 
 shownotification = (msg) => {
-   let toast = document.createElement("div");
-   toast.classList.add("notification-msg");
-   toast.innerHTML = msg;
-   notification.appendChild(toast)
+    let toast = document.createElement("div");
+    toast.classList.add("notification-msg");
+    toast.innerHTML = msg;
+    notification.appendChild(toast)
 
-   if (msg.includes("cannot")||msg.includes("email")){
-    toast.classList.add("warning")
-   }
-   if(msg.includes("correct")){
-    toast.classList.add("error")
-   }
+    if (msg.includes("cannot") || msg.includes("email")) {
+        toast.classList.add("warning")
+    }
+    if (msg.includes("correct")) {
+        toast.classList.add("error")
+    }
 
-   setTimeout(() => {
-    toast.remove();
-   },5500);
+    setTimeout(() => {
+        toast.remove();
+    }, 5500);
 }
 
-// ============================================ moving image for project sectoon ======================================================
-let project = document.querySelectorAll(".project")
-project.forEach(element => {
-    element.addEventListener("mousemove" ,(dets) => {
-        let diff = dets.clientY - element.getBoundingClientRect().top
-        gsap.to(element.querySelector("img"),{
-            opacity: 1,
-            ease: Power1,
-            y: diff,
-            x: dets.clientX
+// ============================================  project section ======================================================
+document.addEventListener("DOMContentLoaded", () => {
+    let namecontainer = document.querySelector(".project-names");
+    let imagecontainer = document.querySelector(".project-preview");
+    let stack = document.querySelector(".stack-container");
+    let livepreview = document.getElementById("livelink");
+    let sourcepreview = document.getElementById("sourcelink");
+    let discription = document.getElementById("discription")
+
+    // creating image element for preview div
+    let imageelem = document.createElement("img");
+    imageelem.src="./assets/herbal-wonders.webp";
+    imagecontainer.appendChild(imageelem);
+
+    // adding all the name and updating contant for the name clicked
+    data.items.forEach((elem,idx) => {
+        const namediv = document.createElement("div");
+        namediv.className = "names";
+        namediv.innerHTML = elem.name;
+
+        if (idx === 0) {
+            namediv.classList.add("active");
+        }
+        
+
+        // for changing the contant after the user clicks the name of the project
+        namediv.addEventListener("click", function () {
+            // Remove 'active' class from all name divs
+            document.querySelectorAll(".names").forEach(div => div.classList.remove("active"));
+
+            // Add 'active' class to the clicked div
+            namediv.classList.add("active");
+
+            imageelem.src = elem.imageUrl;
+            livepreview.href = elem.livelink;
+            sourcepreview.href = elem.codelink;
+            if (elem.discription == undefined || elem.discription === "") {
+                discription.innerHTML = "there is no discription of this project yet"
+            } else {
+                discription.innerHTML = elem.discription;
+            }
+            gsap.from(imageelem, {
+                opacity: 0,
+                duration: 0.5
+            });
+
+            // Clear the stack container before adding new stack icons
+            stack.innerHTML = "";
+            elem.stack.forEach(stackObj => {
+                let stackicon = document.createElement("i");
+                stackicon.className = stackObj.class;
+                stack.appendChild(stackicon);
+            });
+            // Animate the stack icons to appear one by one
+            const stackIcons = stack.querySelectorAll("i");
+            gsap.from(stackIcons, {
+                opacity: 0,
+                y: -10,
+                duration: 0.5,
+                stagger: 0.2 // Delay between each icon's appearance
+            });
+        });
+
+        // Animate the name container
+        gsap.to(namecontainer, {
+            opacity: 0.8,
+            scale: 1.05,
+            duration: 0.2,
+            yoyo: true,
+            repeat: 1,
         })
-        gsap.to(cursor,{
-            opacity:0,
-            scale:0
-        })
-    })
-    element.addEventListener("mouseleave",function () {
-        gsap.to(element.querySelector("img"),{
-            opacity:0,
-            ease:Power1
-        })
-        gsap.to(cursor,{
-            opacity:1,
-            scale:1
-        })
+
+
+        namecontainer.appendChild(namediv);
     });
-});
+})
