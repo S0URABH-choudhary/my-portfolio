2e1a5a566ebf75d3291e68e3eeda36783d89e658
S0URABH_choudhary <subh7898765@gmail.com>
2024-12-24 17:05:18 +0530

    added new project

--- object.js
+++ object.js
@@ -1,8 +1,14 @@
 const data = {
     items: [
-        { name: "Herbal Wonders", imageUrl: "./assets/herbal-wonders.webp", discription: "<H3>Herbal wonters</h3>- A modern web design project . It showcases a seamless blend of animations and user-friendly navigation </br><h4>Features</h4>-Dynamic Animations </br>-Clean Design </br> -Interactive Elements </br>-Modern Aesthetic ", livelink: "https://animatedwebsite-mocha.vercel.app/", codelink:"https://github.com/S0URABH-choudhary/animatedwebsite", stack:[ { class: "fa-brands fa-html5" },
+        {
+            name: "Herbal Wonders", imageUrl: "./assets/herbal-wonders.webp", discription: "<H3>Herbal wonters</h3>- A modern web design project . It showcases a seamless blend of animations and user-friendly navigation </br><h4>Features</h4>-Dynamic Animations </br>-Clean Design </br> -Interactive Elements </br>-Modern Aesthetic ", livelink: "https://animatedwebsite-mocha.vercel.app/", codelink: "https://github.com/S0URABH-choudhary/animatedwebsite", stack: [{ class: "fa-brands fa-html5" },
             { class: "fa-brands fa-css3-alt" },
-            { class: "fa-brands fa-js" }] },
-        { name: "emplooye management systen", imageUrl: "./asscets/pexels-pixabay-159110.jpg", discription: "this is green tea" }
+            { class: "fa-brands fa-js" }]
+        },
+        {
+            name: "Disney-Carousel", imageUrl: "./assets/Screenshot 2024-12-24 163213.png", discription: "<H3>This scroller</h3> <br/> - It showcases disney characters in a horizontal scroll view ", livelink: "https://disney-characters-scroller.vercel.app/", codelink: "https://github.com/S0URABH-choudhary/Disney-Carousel", stack: [{ class: "fa-brands fa-html5" },
+            { class: "fa-brands fa-css3-alt" },
+            { class: "fa-brands fa-js" }]
+        }
     ],
 };
