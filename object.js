9c706a48eb24f269d546a78afee334607f8be368
S0URABH_choudhary <subh7898765@gmail.com>
2024-12-28 21:48:40 +0530

    update

--- object.js
+++ object.js
@@ -1,7 +1,7 @@
 const data = {
     items: [
         {
-            name: "Herbal Wonders", imageUrl: "./assets/herbal-wonders.webp", discription: "<H3>Herbal wonters</h3>- A modern web design project . It showcases a seamless blend of animations and user-friendly navigation </br><h4>Features</h4>-Dynamic Animations </br>-Clean Design </br> -Interactive Elements </br>-Modern Aesthetic ", livelink: "https://animatedwebsite-mocha.vercel.app/", codelink: "https://github.com/S0URABH-choudhary/animatedwebsite", stack: [{ class: "fa-brands fa-html5" },
+            name: "Herbal Wonders", imageUrl: "./assets/herbal-wonders.webp", discription: "<H3>Herbal wonters</h3>- A modern web design project . It showcases a seamless blend of animations and user-friendly navigation </br><h4>Features</h4>-Dynamic Animations </br>-Clean Design </br> -Interactive Elements </br>-Modern Aesthetic ", livelink: "https://herbal-wonders.vercel.app/", codelink: "https://github.com/S0URABH-choudhary/Herbal-wonders", stack: [{ class: "fa-brands fa-html5" },
             { class: "fa-brands fa-css3-alt" },
             { class: "fa-brands fa-js" }]
         },
